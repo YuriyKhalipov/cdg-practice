ba57569e8366dffe2bc4503705829560aa95e164
YuriyKhalipov <90421113+YuriyKhalipov@users.noreply.github.com>
2021-10-16 14:03:19 +0300

    Update lab2_2_spec.rb

--- lab2/lab2_2_spec.rb
+++ lab2/lab2_2_spec.rb
@@ -3,15 +3,15 @@ require './lab2_2.rb'
 
 RSpec.describe "foobar" do 
   it "if at least one number is 20 - it returns the second number, otherwise it returns the sum of these numbers" do 		
-	expect(foobar(20,1)).to eq(1)
+    expect(foobar(20,1)).to eq(1)
   end
 
   it "if at least one number is 20 - it returns the second number, otherwise it returns the sum of these numbers" do 
-	expect(foobar(5,20)).to eq(5)
+    expect(foobar(5,20)).to eq(5)
   end
 
   it "if at least one number is 20 - it returns the second number, otherwise it returns the sum of these numbers" do 
-	expect(foobar(4,5)).to eq(9)
+    expect(foobar(4,5)).to eq(9)
   end
 end
 
